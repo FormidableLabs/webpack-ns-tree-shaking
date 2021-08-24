7a0850651b983db503f8fe976eb3a48e8cbb561e
Ryan Roemer <ryan.roemer@formidable.com>
2021-08-24 13:27:38 -0700

    Start a config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import * as colors from "./colors/index.js";
+// // NS version
+// import * as colors from "./colors/index.js";
+
+// Named import version
+import { red } from "./colors/index.js";
+const colors = { red };
 
 console.log({
   type: typeof colors.red,
